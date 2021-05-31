5db394b95adee211dc49d9282842887dd2304c3d
randytang <rtang@wyze.com>
2021-05-31 11:20:57 -0700

    apply root level wyze_app_build.gradle for the library

---
+++ Wlibrary/src/main/java/com/wyze/wlibrary/Info.java
@@ -0,0 +1,16 @@
+package com.wyze.wlibrary;
+
+public class Info {
+
+    final static public String TEXT = getBuildInfo();
+
+    private static String getBuildInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Wlibrary build info:").append("\n");
+        sb.append(BuildConfig.CLOUD_NAME).append("\n");
+        sb.append(BuildConfig.BUILD_TYPE).append("\n");
+        sb.append(BuildConfig.WYZE_PLUGIN_VERSION_CODE).append("\n");
+        sb.append(BuildConfig.WYZE_PLUGIN_VERSION_NAME).append("\n");
+        return sb.toString();
+    }
+}
--- app/src/main/java/com/wyze/wversion/MainActivity.java
+++ app/src/main/java/com/wyze/wversion/MainActivity.java
@@ -5,6 +5,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.wyze.wlibrary.Info;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,10 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView infoTv = findViewById(R.id.infoTv);
         infoTv.setText(getBuildInfo());
+        infoTv.append("\n");
+        infoTv.append(Info.TEXT);
     }
 
     private String getBuildInfo() {
         StringBuilder sb = new StringBuilder();
+        sb.append("Wyze app build info:").append("\n");
         sb.append(BuildConfig.CLOUD_NAME).append("\n");
         sb.append(BuildConfig.APPLICATION_ID).append("\n");
         sb.append(BuildConfig.BUILD_TYPE).append("\n");
