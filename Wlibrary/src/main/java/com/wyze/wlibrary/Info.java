11bbaf3a4026d790c8cb49768f4a1bce50a7f8a2
randytang <rtang@wyze.com>
2021-05-31 13:14:38 -0700

    apply remote wyze_base_build.gradle.
    Pitfall: git raw content has 5 minutes cache policy.

--- Wlibrary/src/main/java/com/wyze/wlibrary/Info.java
+++ Wlibrary/src/main/java/com/wyze/wlibrary/Info.java
@@ -10,6 +10,7 @@ public class Info {
         sb.append(BuildConfig.BUILD_TYPE).append("\n");
         sb.append(BuildConfig.WYZE_PLUGIN_VERSION_CODE).append("\n");
         sb.append(BuildConfig.WYZE_PLUGIN_VERSION_NAME).append("\n");
+        sb.append(BuildConfig.BASE_BUILD_GRADLE_NAME).append("\n");
         return sb.toString();
     }
 }
