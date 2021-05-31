f0c0cda6a64b2ae6abbf719f8993a4c1ad1f8b3f
randytang <rtang@wyze.com>
2021-05-31 10:52:02 -0700

    Add a CLOUD_NAME buildConfigField Display build info on a TextView

--- app/src/main/java/com/wyze/wversion/MainActivity.java
+++ app/src/main/java/com/wyze/wversion/MainActivity.java
@@ -1,8 +1,9 @@
 package com.wyze.wversion;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView infoTv = findViewById(R.id.infoTv);
+        infoTv.setText(getBuildInfo());
+    }
+
+    private String getBuildInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BuildConfig.CLOUD_NAME).append("\n");
+        sb.append(BuildConfig.APPLICATION_ID).append("\n");
+        sb.append(BuildConfig.BUILD_TYPE).append("\n");
+        sb.append(BuildConfig.VERSION_NAME).append("\n");
+        sb.append(BuildConfig.VERSION_CODE).append("\n");
+        return sb.toString();
     }
 }
